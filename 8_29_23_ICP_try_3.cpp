14f91f4f1facab854447352a2ccd2431093564d7
AntonioAlberico <143756525+AntonioAlberico@users.noreply.github.com>
2023-08-31 17:25:15 -0400

    This is a git fil upload test
    Just a test.

---
+++ 8_29_23_ICP_try_3.cpp
@@ -0,0 +1,12 @@
+﻿// 8_29_23_ICP_try_3.cpp : Defines the entry point for the application.
+//
+
+#include "8_29_23_ICP_try_3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Antonio Alberico" << endl;
+	return 0;
+}
